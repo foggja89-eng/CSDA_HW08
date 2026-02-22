ede53c75b6b13d75e06d5da658a65e145063a69c
foggja89-eng <foggja89@students.rowan.edu>
2026-02-22 10:47:31 -0500

    initial push with assignment description

---
+++ push.sh
@@ -0,0 +1,18 @@
+# push.sh -- use this for a quick way to push changes for this project. 
+# assumes that you have already used global credential configuration for github on this machine so you don't have to enter a username or password.  
+# Author: James C. Fogg
+# Version: 2026.02.22
+
+echo 'adding files...'
+echo
+git add .
+echo
+echo 'adding commit with message...'
+echo
+git commit -m 'initial push with assignment description' # TODO: this will need to be changed every time. 
+echo
+echo 'pushing to main...'
+echo
+git push origin main
+echo
+echo '=====END OF PUSH.SH====='
